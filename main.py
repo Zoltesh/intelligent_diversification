9807dde30af54f4a4cabcf7fc2be8636d6e8cd19
Zoltesh <braydude15@gmail.com>
2026-01-13 21:10:06 -0700

    Fixed issue where the data was scraped using my system datetime (UTC -7:00:00 instead of UTC 00:00:00). Now the data is properly collected from 2024-01-01 00:00:00 to 2025-12-31 23:55:00 UTC 00:00:00.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import asyncio
 import ccxt.async_support as ccxt
+import os
 import polars as pl
 
 from src.exchange.get_ohlcv import fetch_and_write_ohlcv
@@ -14,8 +15,8 @@ SYMBOLS = ["ADA", "AVAX", "BCH", "BTC", "DOGE", "ETH", "LINK", "LTC", "SOL", "XR
 QUOTE = "USDC"
 
 timeframe = '5m'
-since = 1704092400000
-end = 1767250799000
+since = 1704067200000 # 2024-01-01 00:00:00
+end = 1767225300000 # 2025-12-31 23:55:00
 
 async def process_data(symbol: str):
     async with fetch_sem:
@@ -26,6 +27,19 @@ async def process_data(symbol: str):
         since_ms=since,
         end_ms=end,
     )
+
+def drop_tail():
+    # For each parquet, drop any records greater than timestamp == 1767164400000
+    for file in os.listdir('src/data/'):
+        lf = pl.scan_parquet(f'src/data/{file}')
+        df = lf.collect()
+        print(len(df))
+        df_filtered = df.filter(~(pl.col("timestamp") > 1767164400000))
+        print(f'{file}: {len(df_filtered)}')
+
+        # Overwrite the file with the filtered data
+        df_filtered.write_parquet(f'src/data/{file}')
+
 
 async def main():
     coroutines = [process_data(symbol) for symbol in SYMBOLS]
@@ -39,11 +53,13 @@ def read():
     # Also print total number of rows across all symbols
     total_rows = 0
     for symbol in SYMBOLS:
-        lf = pl.scan_parquet(f"data_cleaned/{symbol}-USDC.parquet")
+        lf = pl.scan_parquet(f"src/data/{symbol}-USDC.parquet")
         df = lf.collect()
-        print(f"{symbol} {df.shape}")
+        perc = (210_528 - len(df))/210_528 * 100
+        print(f'{symbol} {len(df)} {perc:.2f}% {df['timestamp'].min()} {df['timestamp'].max()}')
         total_rows += df.shape[0]
     print(f"Total number of rows across all symbols: {total_rows}")
+
 
 if __name__ == "__main__":
     read()
